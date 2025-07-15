681d87a4a143c9b17d16c7890c223594d4e7ad21
FIXED-TERM Vinaik Suvab (BGSW/PJ-ETA-P) <qiu3kor@bosch.com>
2025-07-15 16:39:59 +0530

    product classes - 2

---
+++ backend/OrderKraft/src/main/java/com/entity/ProductionUnit.java
@@ -0,0 +1,56 @@
+package com.entity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "production_unit")
+public class ProductionUnit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "production_unit_seq")
+    @SequenceGenerator(name = "production_unit_seq", sequenceName = "production_unit_seq", allocationSize = 1)
+    private Long unitId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "capacity")
+    private int capacity;
+
+    @ManyToOne
+    @JoinColumn(name = "production_manager_id")
+    private User user;
+
+    // Getters and Setters
+
+    public Long getUnitId() {
+        return unitId;
+    }
+
+    public void setUnitId(Long unitId) {
+        this.unitId = unitId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
