5fccbf450ac130f9954d207d6bbd6ab8757672cb
FIXED-TERM Vinaik Suvab (BGSW/PJ-ETA-P) <qiu3kor@bosch.com>
2025-07-18 11:21:30 +0530

    supplierPerformance

--- backend/OrderKraft/src/main/java/com/controller/RawMaterialController.java
+++ backend/OrderKraft/src/main/java/com/controller/RawMaterialController.java
@@ -2,6 +2,7 @@ package com.controller;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dto.Raw_MaterialDTO;
-import com.dto.SupplierDTO;
+
 import com.entity.RawMaterial;
-import com.entity.Supplier;
+
 import com.service.RawMaterialService;
-import com.service.SupplierService;
+
 
 @RestController
 @RequestMapping("/raw_material")
---
+++ backend/OrderKraft/src/main/java/com/controller/SupplierPerformanceController.java
@@ -0,0 +1,38 @@
+package com.controller;
+
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.dto.Supplier_PerformanceDTO;
+import com.entity.SupplierPerformance;
+import com.service.SupplierPerformanceService;
+
+@RestController
+@RequestMapping("/supplier_performance")
+public class SupplierPerformanceController {
+	
+	@Autowired
+	SupplierPerformanceService supplierPerformanceService;
+	
+	@PostMapping("/add")
+	public ResponseEntity<SupplierPerformance> createRawMaterial(@RequestBody Supplier_PerformanceDTO supplierPerformancedto)
+	{
+		return ResponseEntity.ok(supplierPerformanceService.createSupplierPerformance(supplierPerformancedto));
+		
+	}
+	
+	@GetMapping("/all")
+	public ResponseEntity<List<SupplierPerformance>> fetchAll()
+	{
+		return ResponseEntity.ok(supplierPerformanceService.getAllSupplierPerformance());
+	}
+}
