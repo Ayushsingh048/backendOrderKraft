1a4e157d444f7077fa75bdd5ee0085f3f069da2f
FIXED-TERM Vinaik Suvab (BGSW/PJ-ETA-P) <qiu3kor@bosch.com>
2025-07-25 14:53:36 +0530

    Password encoder v3

--- frontend/OrderKraft-frontend/src/app/auth-guard.ts
+++ frontend/OrderKraft-frontend/src/app/auth-guard.ts
@@ -1,26 +1,9 @@
 import { inject, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { CanActivateFn, Router } from '@angular/router';
-import { AuthService } from './auth.service';
-import { PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const platformId = inject(PLATFORM_ID);
-<<<<<<< HEAD
-
-  if(isPlatformBrowser(platformId)){
-  const isLoggedIn = inject(AuthService).getToken() !== null;
-console.log("isLoggedIn"+isLoggedIn+" "+localStorage.getItem('authToken'));
-  if (!isLoggedIn) {
-    // redirect to login
-    const router = inject(Router);
-    router.navigate(['/login']);
-    return false;
-  }
-}
-=======
->>>>>>> 562a465eeb9a541ed1ce930b3510dfd009fef285
 
   if (isPlatformBrowser(platformId)) {
     const isLoggedIn = localStorage.getItem('authToken') !== null;
