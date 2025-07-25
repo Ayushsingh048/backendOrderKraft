e9edf39c5fd83c7b455b8ce96da2d10e22560059
FIXED-TERM Singh Ayush (BGSW/PJ-ETA-P) <byg2kor@bosch.com>
2025-07-25 10:49:00 +0530

    skipping ssr at time of refresh

--- frontend/OrderKraft-frontend/src/app/auth-guard.ts
+++ frontend/OrderKraft-frontend/src/app/auth-guard.ts
@@ -1,15 +1,24 @@
-import { inject } from '@angular/core';
+import { inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  const isLoggedIn = localStorage.getItem('authToken') !== null;
-console.log("isLoggedIn"+isLoggedIn+" "+localStorage.getItem('authToken'));
-  if (!isLoggedIn) {
-    // redirect to login
-    const router = inject(Router);
-    router.navigate(['/login']);
-    return false;
-  }
+  const platformId = inject(PLATFORM_ID);
 
-  return true;
+  if (isPlatformBrowser(platformId)) {
+    const isLoggedIn = localStorage.getItem('authToken') !== null;
+    console.log("isLoggedIn: " + isLoggedIn + " Token: " + localStorage.getItem('authToken'));
+
+    if (!isLoggedIn) {
+      const router = inject(Router);
+      router.navigate(['/login']);
+      return false;
+    }
+
+    return true;
+  } else {
+    // During SSR: prevent accessing localStorage
+    console.warn("SSR: Skipping auth guard check");
+    return true; // optionally true if you want to render login first on server
+  }
 };
