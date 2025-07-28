acbc35b78430bf65fb0c1c04fa0f123da6961473
Badugu Ashajyothi <fixed-term.Badugu.AshaJyothi@bosch.com>
2025-07-28 14:27:45 +0530

    updated

--- frontend/OrderKraft-frontend/src/app/app.routes.ts
+++ frontend/OrderKraft-frontend/src/app/app.routes.ts
@@ -1,33 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginPage } from './login-page/login-page';
 import { Test } from './test/test';
-<<<<<<< HEAD
 import { ProductionManagerPage } from './production-manager/production-manager';
-
-export const routes: Routes = [{
-    path: 'login',component: LoginPage},
-  // {
-  //   path: '',
-  //   redirectTo: '/login',
-  //   pathMatch: 'full'
-  // },
-  {path:'test',component:Test},
-  { path: 'manager', component: ProductionManagerPage }, // ✅ add route
-  { path: '', redirectTo: '/manager', pathMatch: 'full' } // ✅ temporary default
-];
-=======
 import { authGuard } from './auth-guard';
 export const routes: Routes = [{
-    path: 'login',
-    component: LoginPage
-  },
-  {
-    path: '',
-    redirectTo: '/login',
-    pathMatch: 'full'
-  },{
-    path:'test',
-    component:Test,
-    canActivate: [authGuard]
-  }];
->>>>>>> 86979af252453d82bcc5fdeeb12781f3028a47e5
+    path: 'login',component: LoginPage},
+  {path: '',redirectTo: '/login',pathMatch: 'full'},
+  { path:'test',component:Test,canActivate: [authGuard]},
+   { path: 'manager', component: ProductionManagerPage }
+
+];
+
+
+
