221e8a70c63a4b41aa1e2d07ee1641a5b75e43b3
FIXED-TERM Tripathi Satya Sindhu (BGSW/PJ-ETA-P) <lsp3kor@bosch.com>
2025-07-29 16:41:08 +0530

    user registration field update

--- frontend/OrderKraft-frontend/src/app/app.routes.ts
+++ frontend/OrderKraft-frontend/src/app/app.routes.ts
@@ -22,11 +22,7 @@ export const routes: Routes = [{
   path:"forgotPassword",
   component :ForgotPasswordPage
 },
-<<<<<<< HEAD
-{
-=======
   {
->>>>>>> aa52a7e6f895f9cfa39dbd4c17d838812a08e332
   path: 'user-registration',
   component:UserRegistration,
   // canActivate: [authGuard]
@@ -34,7 +30,3 @@ export const routes: Routes = [{
 
 }
 ];
-<<<<<<< HEAD
-  
-=======
->>>>>>> aa52a7e6f895f9cfa39dbd4c17d838812a08e332
