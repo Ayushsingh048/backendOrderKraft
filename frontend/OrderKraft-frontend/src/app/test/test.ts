889c6bb8615c101acd75012a6be50deb71a0b93c
FIXED-TERM Singh Ayush (BGSW/PJ-ETA-P) <byg2kor@bosch.com>
2025-08-28 16:48:32 +0530

    payment table update added

--- frontend/OrderKraft-frontend/src/app/test/test.ts
+++ frontend/OrderKraft-frontend/src/app/test/test.ts
@@ -12,7 +12,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 })
 export class Test {
  refundOrder: string = "";
-
+//refund function
 getRefund() {
   this.http.post<string>(`http://localhost:8081/payments/refund/${this.refundOrder}`, {})
     .subscribe({
@@ -29,7 +29,7 @@ getRefund() {
 
 oid: any;
 private baseUrl = "http://localhost:8081/payments/check";
-
+//get status
 getStatus() {
   const url = `${this.baseUrl}?orderid=${this.oid}`;
   this.http.get(url, { withCredentials: true }).subscribe({
