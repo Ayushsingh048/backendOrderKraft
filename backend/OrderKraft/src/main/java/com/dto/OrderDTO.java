c57ee750b3b6bda82ec343ad5c80b1a30a469dd7
Badugu Ashajyothi <fixed-term.Badugu.AshaJyothi@bosch.com>
2025-09-02 09:54:14 +0530

    added order name

--- backend/OrderKraft/src/main/java/com/dto/OrderDTO.java
+++ backend/OrderKraft/src/main/java/com/dto/OrderDTO.java
@@ -8,11 +8,12 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class OrderDTO {
 
-@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
 private LocalDate  order_date;//needs to be formatted, if necessary!
 private String status;
 private BigDecimal total_amount;
 private long procurement_officer_id;
+private String order_name;
 
 public String getStatus() {
 	return status;
@@ -41,15 +42,25 @@ public void setProcurement_officer_id(long procurement_officer_id) {
 	this.procurement_officer_id = procurement_officer_id;
 }
 
+public String getOrder_name() {
+	return order_name;
+}
+public void setOrder_name(String order_name) {
+	this.order_name = order_name;
+}
 //no-args constr.
 public OrderDTO() {}
 
 //all-args constr.
-public OrderDTO(LocalDate order_date, String status, BigDecimal total_amount, long procurement_officer_id) {
+public OrderDTO(LocalDate order_date, String status, BigDecimal total_amount, long procurement_officer_id,
+		String order_name) {
 	super();
 	this.order_date = order_date;
 	this.status = status;
 	this.total_amount = total_amount;
 	this.procurement_officer_id = procurement_officer_id;
+	this.order_name = order_name;
 }
+
+
 }
